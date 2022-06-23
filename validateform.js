bcf1076554760cd5e9065170b6f65d1c3ad78d82
Hasnain <mrajhasnain101@gmail.com>
2022-06-23 15:49:32 +0600

    upload

---
+++ validateform.js
@@ -0,0 +1,16 @@
+const fname=document.getElementById('first-name');
+const lname=document.getElementById('last-name');
+const email=document.getElementById('email');
+const phone=document.getElementById('phonenumber');
+const country =document.getElementById('country');
+const city=document.getElementById('city');
+const password=document.getElementById('password');
+
+const form=document.getElementById('form');
+ 
+form.addEventListener('submit',(e)=>{
+    
+    e.preventDefault();
+})
+
+
