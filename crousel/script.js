7556ee92f749e87cfe6308fce97443df33e40ee2
Hasnain <mrajhasnain101@gmail.com>
2022-10-14 23:00:12 +0600

    setting up the new index

--- crousel/script.js
+++ crousel/script.js
@@ -3,6 +3,8 @@ const buttons=document.querySelectorAll("[data-carousel-button]");
 buttons.forEach(button,()=>{
     button.addEventListener('click',()=>{
         const offset= button.dataset.carouselButton==="prev"?  1:-1;
-        const slides=button.closest("[data-carousel]")
+        const slides=button.closest("[data-carousel]").querySelector("[data-slides]")
+        const activeSlide=slides.querySelectorAll("[data-active]");
+        const newIndex=[...slides.children].indexOf()
     })
 })
