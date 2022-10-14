7be05485667f02ebef3334ab6b013bd2b1b784e1
Hasnain <mrajhasnain101@gmail.com>
2022-10-15 00:21:14 +0600

    finally done

--- crousel/script.js
+++ crousel/script.js
@@ -1,10 +1,16 @@
 const buttons=document.querySelectorAll("[data-carousel-button]");
 
-buttons.forEach(button,()=>{
+buttons.forEach(button=>{
     button.addEventListener('click',()=>{
-        const offset= button.dataset.carouselButton==="prev"?  1:-1;
-        const slides=button.closest("[data-carousel]").querySelector("[data-slides]")
-        const activeSlide=slides.querySelectorAll("[data-active]");
-        const newIndex=[...slides.children].indexOf()
+        const offset= button.dataset.carouselButton === "next"?  1 : -1;
+        const slides=button.closest("[data-carousel]").querySelector("[data-slides]");
+
+        const activeSlide=slides.querySelector("[data-activate]");
+        let newIndex=[...slides.children].indexOf(activeSlide)+offset;
+        if(newIndex<0)newIndex=slides.children.length-1;        
+        if(newIndex>=slides.children.length) newIndex=0;   
+
+        slides.children[newIndex].dataset.activate=true;
+        delete activeSlide.dataset.activate;
     })
 })
