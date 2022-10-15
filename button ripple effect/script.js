4437e29e51428b92e2bbec8be21d5a1659d0d63f
Hasnain <mrajhasnain101@gmail.com>
2022-10-15 23:28:34 +0600

    js done

--- button ripple effect/script.js
+++ button ripple effect/script.js
@@ -1,6 +1,10 @@
-const btnEle=document.querySelector('.buttonElement');
+const btnEle=document.querySelector('.btnElement');
 
 btnEle.addEventListener('mouseover',(e)=>{
-    console.log("")
+    const x=(e.pageX-btnEle.offsetLeft);
+    const y=(e.pageY-btnEle.offsetTop);
+
+    btnEle.style.setProperty("--posX",x +"px");
+    btnEle.style.setProperty("--posY",y +"px");
 
 })
