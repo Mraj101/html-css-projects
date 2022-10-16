29d71f35814ba44f20d5a5507980327a1994d7af
Hasnain <mrajhasnain101@gmail.com>
2022-10-16 23:47:53 +0600

    js were added succesfully

--- img scroll effect/script.js
+++ img scroll effect/script.js
@@ -1,1 +1,10 @@
 //starting here
+const bgElement=document.getElementById("img-Id");
+
+window.addEventListener('scroll',()=>{
+    let opacityPercentage=1-window.pageYOffset/600;
+    let bgSize=100-window.pageYOffset/60;
+    console.log(bgSize)
+    bgElement.style.opacity=opacityPercentage
+    bgElement.style.backgroundSize=bgSize+"%";
+})
